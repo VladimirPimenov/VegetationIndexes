1e3880254b3119b49e60dea0ce49b49255c08d34
Vladimir <pimenovv2000@gmail.com>
2025-04-09 01:27:23 +0300

    Добавлен метод для получения каналов по диапазону длин волн

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import numpy as np
+
 from tifFileHandler import saveTifAsMat, readHSIChannelFromMat
 from xmlPassportParser import readTableLengthWave
 
-hsiPath = "./0041_0306_34728_1_04894_06_L1A.tif"
-matSavePath = "./hsi.mat"
+def getChannelNumsByWavesRange(startWaveLength, endWaveLength, channelTable):
+    channels = list()
 
-passportFilePath = "./0041_0306_34728_1_04894_06_L1A.xml"
-table = readTableLengthWave(passportFilePath)
+    for waveLength in channelTable.keys():
+        if(startWaveLength <= waveLength <= endWaveLength):
+            channels.append(channelTable[waveLength])
 
-print(table)
+    return channels
+
+def main():
+    hsiPath = "./0041_0306_34728_1_04894_06_L1A.tif"
+    matSavePath = "./hsi.mat"
+    passportFilePath = "./0041_0306_34728_1_04894_06_L1A.xml"
+
+    channelTable = readTableLengthWave(passportFilePath)
+
+    redChannelNums = getChannelNumsByWavesRange(630, 750, channelTable)
+    nirChannelNums = getChannelNumsByWavesRange(750, 1400, channelTable)
+    #hsiShape = (2388, 999)
+
+    print(redChannelNums)
+    print(nirChannelNums)
+
+if __name__ == "__main__":
+    main()
