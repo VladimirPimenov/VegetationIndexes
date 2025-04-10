a495c3be0511da9abcd8b8744cf156eda46265ab
Vladimir Pimenov <167480900+VladimirPimenov@users.noreply.github.com>
2025-04-10 21:20:27 +0300

    Рефакторинг

--- indexesCalculator/ndvi.py
+++ indexesCalculator/ndvi.py
@@ -3,18 +3,15 @@ import numpy as np
 def calculateNDVIforChannels(redChannels: np.ndarray, nirChannels: np.ndarray) -> np.ndarray:
     NDVIimage = np.zeros((redChannels.shape[1], redChannels.shape[2]))
     
-    channelCount = redChannels.shape[0]
     height = redChannels.shape[1]
     width = redChannels.shape[2]
 
-    for channelNum in range(channelCount):
-        for y in range(height):
-            for x in range(width):
-                redAvgPixelValue = calculateAvgPixelValue(x, y, redChannels)
-                nirAvgPixelValue = calculateAvgPixelValue(x, y, nirChannels)
+    for y in range(height):
+        for x in range(width):
+            redAvgPixelValue = calculateAvgPixelValue(x, y, redChannels)
+            nirAvgPixelValue = calculateAvgPixelValue(x, y, nirChannels)
 
-                NDVIpixel = calculateNDVI(redAvgPixelValue, nirAvgPixelValue)
-                NDVIimage[y][x] = 100 * (NDVIpixel + 1)
+            NDVIimage[y][x] = calculateNDVI(redAvgPixelValue, nirAvgPixelValue)
 
     return NDVIimage
 
