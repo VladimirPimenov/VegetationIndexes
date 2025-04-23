c51f4421384c20b0e6e4e7f0905b030b433cfbda
Vladimir Pimenov <167480900+VladimirPimenov@users.noreply.github.com>
2025-04-24 00:54:20 +0300

    Реализован расчёт SAVI для диапазона длин волн

--- main.py
+++ main.py
@@ -105,6 +105,25 @@ def simpleSAVIcalculating(redChannelNum, nirChannelNum):
 
     plt.imshow(savi)
     plt.show()
+def rangeSAVIcalculating(redWavesRange, nirWavesRange):
+    redChannelNums = getChannelNumsByWavesRange(redWavesRange[0], redWavesRange[1])
+    redChannelNums = orderChannelNums(redChannelNums)
+
+    nirChannelNums = getChannelNumsByWavesRange(nirWavesRange[0], nirWavesRange[1])
+    nirChannelNums = orderChannelNums(nirChannelNums)
+
+    print(f"Red channels: {redChannelNums}")
+    print(f"Nir channels: {nirChannelNums}")
+
+    redChannels = np.zeros((len(redChannelNums), 2388, 999))
+    nirChannels = np.zeros((len(nirChannelNums), 2388, 999))
+    fillChannelsFromMat(redChannels, redChannelNums)
+    fillChannelsFromMat(nirChannels, nirChannelNums)
+
+    savi = indexesCalculating.calculateSAVIforChannels(redChannels, nirChannels, 0.5)
+
+    plt.imshow(savi)
+    plt.show()
 
 def fillChannelsFromMat(channels, channelNums):
     currentChannel = 0
@@ -119,10 +138,11 @@ def main():
     #simpleNDVIcalculating(72, 85)
     #rangeNDVIcalculating([630, 635], [750, 755])
 
-    simpleARVIcalculating(75, 85, 14)
+    #simpleARVIcalculating(75, 85, 14)
     #rangeARVIcalculating([630, 750], [750, 1400], [440, 485])
 
-    #simpleSAVIcalculating(72, 85)
+    simpleSAVIcalculating(72, 85)
+    rangeSAVIcalculating([630, 750], [750, 1400])
 
 if __name__ == "__main__":
     main()
