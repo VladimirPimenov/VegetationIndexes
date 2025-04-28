7f32d70d8e91ac5d98fd6186004187b45f0cbc79
Vladimir Pimenov <167480900+VladimirPimenov@users.noreply.github.com>
2025-04-29 01:39:29 +0300

    Добавлена прозрачность маски

--- maskCreator.py
+++ maskCreator.py
@@ -4,12 +4,12 @@ def createMaskFromNDVI(ndvi: np.ndarray) -> np.ndarray:
     height = ndvi.shape[0]
     width = ndvi.shape[1]
 
-    mask = np.zeros((height, width, 3), dtype = np.uint8)
+    mask = np.zeros((height, width, 4), dtype = np.uint8)
 
     for y in range(height):
         for x in range(width):
             if(0.2 <= ndvi[y][x] < 0.4):
-                mask[y][x] = [255, 0, 0]
+                mask[y][x] = [255, 0, 0, 200]
             else:
-                mask[y][x] = [255, 255, 255]
+                mask[y][x] = [0, 0, 0, 0]
     return mask
